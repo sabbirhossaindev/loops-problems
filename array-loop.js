735dd4251bb78c194009f2e1e2ab8a8268c55abd
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-02 06:31:21 +0600

    length

---
+++ array-loop.js
@@ -0,0 +1,12 @@
+// for loop
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// target : display every element of an array. // .length
+var numbers = [45, 87, 89, 32, 99, 80, 21, 188, 33, 55, 22];
+
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    console.log(number);
+}
