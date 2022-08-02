8f4f6738ce2719bd8a8b2568ee779d0ce323293b
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-02 07:03:14 +0600

    revers loop

---
+++ reverse.js
@@ -0,0 +1,11 @@
+// target : 10 hota 1 run kora; // while loop diya
+var num = 10;
+while (num >= 1) {
+    console.log(num);
+    num--;
+}
+
+// target : 10 hota 1 run kora; // for loop diya
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
