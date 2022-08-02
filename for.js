0dc46b52be9c9d4f520da4d159f560a711f5d2f7
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-02 06:17:57 +0600

    all done

---
+++ for.js
@@ -0,0 +1,27 @@
+var rostGiven = 0;
+while (rostGiven < 7) {
+    console.log('Rost dan pls!!!!');
+    console.log(rostGiven);
+    rostGiven++;
+}
+
+for (var rostGiven = 0; rostGiven < 7; rostGiven++) {
+    console.log('rost koi');
+}
+
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// jow sonla
+// i += 2;
+for (var i = 0; i <= 10; i+=2) {
+    console.log(i);
+}
+
+
+// bijow sonla
+// i += 2;
+for (var i = 1; i <= 10; i+=2) {
+    console.log(i);
+}
